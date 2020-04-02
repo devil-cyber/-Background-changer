c3747a6f946a8f649b86df8b7c591e9c6da6bbd4
Manikant Kumar <52333306+devil-cyber@users.noreply.github.com>
2020-04-02 09:35:41 +0530

    Update code.js

--- code.js
+++ code.js
@@ -3,13 +3,23 @@ var count=0;
 input.addEventListener("click",function(){
     count++;
     if(count%2!=0){
+          var color="#";
+    var symbol="0123456789ABCDEF";
+    for(var i=0;i<6;i++){
+        color=color+symbol[Math.floor(Math.random()*16)];
+    }
            var back=document.getElementById("body");
-    back.style.backgroundColor="black";
-    document.getElementById("para").style.color="white";
+    back.style.backgroundColor=color;
+  
     }
     else{
            var back=document.getElementById("body");
-    back.style.backgroundColor="white";
-    document.getElementById("para").style.color="black";
+          var color="#";
+    var symbol="0123456789ABCDEF";
+    for(var i=0;i<6;i++){
+        color=color+symbol[Math.floor(Math.random()*16)];
+    }
+    back.style.backgroundColor=color;
+
     }
 });
